6fe74d5d7ecf44f371eb66ba98324d6192bddbf2
Arran Oakes <arran.oakes@gmail.com>
2021-05-27 11:46:19 +0100

    we need more functionality I'm afraid

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,14 +9,20 @@ describe 'fizzbuzz' do
   end
 
   it 'returns "fizz" when passed multiple of 3' do
-    [6,9,12,15,18,21].each { |num| 
-      expect(fizzbuzz(num)).to eq 'fizz'    
+    [6,9,12,15,18,21].each { |num|
+      expect(fizzbuzz(num)).to eq 'fizz'
     }
+  end
+
+  it 'returns "buzz" when passed multiples of 5' do
+    [5,10,15,20,100].each do |num|
+      expect(fizzbuzz(num)).to eq "buzz"
+    end
   end
 end
 # RSpec.describe "#answer" do
 #   expected_values = {'3': 'Fizz', '5': 'Buzz', '6': 'Fizz', '11': '11', '15': 'FizzBuzz'}
-#   expected_values.each do |val, expected| 
-#     it "returns #{expected} when number is #{val}" do 
+#   expected_values.each do |val, expected|
+#     it "returns #{expected} when number is #{val}" do
 #         result = FizzBuzz.new.answer(val.to_i)
 #         expect(result).to eq(expected)
